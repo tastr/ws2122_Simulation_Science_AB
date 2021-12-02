ec2a9bcfe5979f9156e3e9d6f0200fe30b526fef
tastr <Strelnikovatatyana@gmail.com>
2021-12-02 02:36:30 +0100

    feat: add joints

--- Simulink_Model/init_model_parameter.m
+++ Simulink_Model/init_model_parameter.m
@@ -1,3 +1,5 @@
+clear all
+
 %% dimensions
 
 % foots
@@ -9,7 +11,7 @@ bodyparam.size.lowerleg = [.01 .01  0.3];
 bodyparam.size.leg = [bodyparam.size.lowerleg(1:2), bodyparam.size.lowerleg(3) + bodyparam.size.upperleg(3)];
 
 % hip 
-bodyparam.size.hip = [.25 .25 .01];
+bodyparam.size.hip = [.25 .01 .01];
 
 % body
 bodyparam.size.body = [.01 .01 .7];
@@ -29,22 +31,35 @@ bodyparam.size.head  = .1;% radius
 %% Transormations:
                                        
 % foots
-bodyparam.position.foot_right_trans     = [bodyparam.size.hip(1)/2 + bodyparam.size.foot(1)/2, 0, bodyparam.size.foot(3)/2];
-bodyparam.position.foot_left_trans      = [bodyparam.size.hip(1)/2 + bodyparam.size.foot(1)/2, 0, -bodyparam.size.foot(3)/2];
+bodyparam.position.foot_right_trans.joint     = [-bodyparam.size.hip(1)/2, 0, -bodyparam.size.foot(3)/2];
+bodyparam.position.foot_right_trans.item     =  [-bodyparam.size.foot(1)/2, 0, 0];
+
+bodyparam.position.foot_left_trans.joint      = [0, 0, -bodyparam.size.lowerleg(3)/2];
+bodyparam.position.foot_left_trans.item       = [-bodyparam.size.foot(1)/2, 0, 0]
+
 
 % lower legs
-bodyparam.position.lowerleg_right_trans = [bodyparam.size.hip(1)/2, 0, bodyparam.size.lowerleg(3)/2];
-bodyparam.position.lowerleg_left_trans  = [-bodyparam.size.hip(1)/2, 0,bodyparam.size.lowerleg(3)/2];
+bodyparam.position.lowerleg_right_trans.item   = [0, 0, -bodyparam.size.lowerleg(3)/2]
+bodyparam.position.lowerleg_right_trans.joint  = [0, 0, -bodyparam.size.lowerleg(3)/2];
+
+bodyparam.position.lowerleg_left_trans.item    = [0, 0, bodyparam.size.lowerleg(3)/2];
+bodyparam.position.lowerleg_left_trans.joint   = [0, 0, bodyparam.size.upperleg(3)/2];
 
 % upper legs
-bodyparam.position.upperleg_right_trans = [0, 0, bodyparam.size.upperleg(3)/2];
-bodyparam.position.upperleg_left_trans  = [0, 0, bodyparam.size.upperleg(3)/2];
+bodyparam.position.upperleg_right_trans.item =  [0, 0, -bodyparam.size.upperleg(3)/2];
+bodyparam.position.upperleg_right_trans.joint = [0, 0, -bodyparam.size.upperleg(3)/2];
+
+bodyparam.position.upperleg_left_trans.joint =  [bodyparam.size.hip(1)/2 - bodyparam.size.upperleg(1)/2, 0, 0];
+bodyparam.position.upperleg_left_trans.item =   [0, 0, bodyparam.size.upperleg(3)/2];
+
+
+bodyparam.jointspos.upperleg_hip = [0, 0, -bodyparam.size.upperleg(3)/2];
 
 % hip
-bodyparam.position.hip_trans  = [-bodyparam.size.hip(1)/2, 0, body.param.size.hip(3)/2 + bodyparam.size.upperleg(3)/2];
+bodyparam.position.hip_trans  = [bodyparam.size.hip(1)/2 -  bodyparam.size.lowerleg(1)/2, 0, -bodyparam.size.hip(3)/2];
 
 % body
-bodyparam.position.body_trans = [0 0 bodyparam.size.body(3)/2];
+bodyparam.position.body_trans = [0 0 -bodyparam.size.body(3)/2];
 
 % arms
 bodyparam.position.upperarms_right_trans = [0 0 bodyparam.size.body(3)/2-bodyparam.size.neck(3)]
@@ -55,6 +70,8 @@ bodyparam.position.lowerarms_left_trans = [-bodyparam.size.upperarms(1)/2 0 -bod
 
 % head
 bodyparam.position.head = [0 0 .5*bodyparam.size.body(3) + .5*bodyparam.size.head];
+
+
 
 %% Init stuff parameters
 
