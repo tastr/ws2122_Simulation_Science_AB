8a99941aef15c29deb7d73a0e406577459c2ff96
tastr <Strelnikovatatyana@gmail.com>
2021-12-02 17:13:57 +0100

    change weight and stiffness parameters

--- Simulink_Model/init_model_parameter.m
+++ Simulink_Model/init_model_parameter.m
@@ -3,11 +3,11 @@ clear all
 %% dimensions
 
 % foots
-bodyparam.size.foot = [.1 .15 .01]; 
+bodyparam.size.foot = [.1 .2 .01]; 
 
 %legs
-bodyparam.size.upperleg = [.01 .01 0.45];
-bodyparam.size.lowerleg = [.01 .01  0.3];
+bodyparam.size.upperleg = [.01 .01 0.41];
+bodyparam.size.lowerleg = [.01 .01  0.415];
 bodyparam.size.leg = [bodyparam.size.lowerleg(1:2), bodyparam.size.lowerleg(3) + bodyparam.size.upperleg(3)];
 
 % hip 
@@ -80,8 +80,48 @@ stuffparam.dimensions.ball = .1; %[m] radius
 stuffparam.transformation.body_wall_trans = [4 0 0];
 
 stuffparam.dimensions.floor = [4,4,.0001];
-stuffparam.transformation.floor = [0 0 .5*stuffparam.dimensions.floor(3)];
+stuffparam.transformation.floor = [0 0 -.5*stuffparam.dimensions.floor(3)];
 
+
+%% weights according to https://exrx.net/Kinesiology/Segments for total 62kg
+tw = 62; %total weight
+bodyparam.weight.total = tw; %[kg]
+bodyparam.weight.head = .0826*tw; 
+bodyparam.weight.upperarm = .0325*tw;
+bodyparam.weight.lowerarm = 0.0252*tw; 
+bodyparam.weight.hip = .1366*tw; 
+bodyparam.weight.upperleg = .105*tw;
+bodyparam.weight.lowerleg = 0.0475*tw; 
+bodyparam.weight.foot = 0.0143*tw; 
+bodyparam.weight.body = bodyparam.weight.total - bodyparam.weight.head...
+                                   - bodyparam.weight.upperarm...
+                                   - bodyparam.weight.lowerarm...
+                                   - bodyparam.weight.hip...
+                                   - bodyparam.weight.upperleg...
+                                   - bodyparam.weight.lowerleg...
+                                   - bodyparam.weight.foot;
+
+%% stiffness and damping
+
+% femur
+bodyparam.stiffness.femur = 7;%[Nm/rad] No ref
+bodyparam.damping.femur = .2;%[Nms/rad]
+
+bodyparam.stiffness.knee = 10.72;%[Nm/rad] according to jospt.2006.2320
+bodyparam.damping.knee = 0.29;%[Nms/rad]
+
+% TODO: 
+% pelvis
+% bodyparam.stiffness.pelvis = 10.72;%[Nm/rad] according to jospt.2006.2320
+% bodyparam.damping.pelvis = 0.29;%[Nms/rad]
+
+% foot
+% bodyparam.stiffness.foot = 10.72;%[Nm/rad] according to jospt.2006.2320
+% bodyparam.damping.foot = 0.29;%[Nms/rad]
+
+% Next steps: 
+% Muscels 
+% Interaction with the ball 
 
 
 
